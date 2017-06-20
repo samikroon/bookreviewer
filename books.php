f72863a5b3d57e4a713bb612f4383e1b5b7e6fb7
samikroon <samikroon@gmail.com>
2017-06-20 12:35:45 +0200

    made add book working, placement needs changing though

--- books.php
+++ books.php
@@ -10,7 +10,9 @@ color:#00FF00;>
 		
 	</div>
 	<div class="row">
-		<div class="book-column col-md-4">test</div>
+		<div class="book-column col-md-4" id="foto">
+			
+		</div>
 		<div class="book-column col-md-4">test</div>
 		<div class="book-column col-md-4">test</div>
 		<div class="book-column col-md-4">test</div>
@@ -21,6 +23,24 @@ color:#00FF00;>
 
 	
 </div>
+<script>
+	$(window).on('load', function loadFoto() {
+		var img = new Image();
+		alert("jemoeder");
+		$.ajax({
+				type: "get",
+				url: "http://37.97.227.173:5000/download",
+				headers: {
+					'path' : '/home/samikroon/photoLibrary/hermanKoch.jpg'
+				},
+				dataType: "file",
+				success: function(data) {
+					$('#foto').prepend('<img id=\"defoto\" src=\"'+data+'\" />"');
+
+				}
+			});
+	});
+</script>
 
 </body>
 </html>
